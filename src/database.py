155c11ee0cd5be821a601e7a74ba3ba2bd28dda6
Oliver0804 <icetzsr@gmail.com>
2023-10-21 13:19:44 +0800

    updata database create tabel

--- src/database.py
+++ src/database.py
@@ -20,6 +20,8 @@ def init_db_connection():
     )
     db_cursor = db_connection.cursor()
     atexit.register(close_db_connection)
+    
+    create_table()
 
 def close_db_connection():
     global db_connection, db_cursor
@@ -30,19 +32,17 @@ def close_db_connection():
 
 def create_table():
     global db_connection, db_cursor
-    # 確保數據庫連接已經初始化
     if db_connection is None or db_cursor is None:
         print("Database connection has not been initialized. Call init_db_connection first.")
         return
     
-    # 您可以根據需要修改以下SQL語句
     create_table_sql = """
     CREATE TABLE IF NOT EXISTS HeartbeatData (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        mac_address VARCHAR(255) NOT NULL,
-        heartbeat_value INT NOT NULL,
-        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+        mac_address VARCHAR(20) NOT NULL,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        heartbeat INT NOT NULL
+        );
     """
     
     try:
